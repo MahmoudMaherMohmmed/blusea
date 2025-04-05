37f0d49597460a439779aae9d7e2bd9249a8de95
Mahmoud Maher <mahmoud_maher12345@yahoo.com>
2025-04-05 15:59:53 +0200

    update services

--- resources/views/dashboard/services/form.blade.php
+++ resources/views/dashboard/services/form.blade.php
@@ -72,7 +72,7 @@
 
                             <div class="col-12">
                                 <div class="form-group">
-                                    <label class="form-label">{{ __('services.attributes.description') }} <span class="tx-danger">*</span></label>
+                                    <label class="form-label">{{ __('services.attributes.description') }}</label>
                                     <div class="example">
 										<div class="panel panel-primary tabs-style-1">
                                             <div class=" tab-menu-heading">
@@ -104,7 +104,7 @@
                                 <div class="form-group">
                                     <label class="form-label">{{ __('services.attributes.icon') }} <span class="tx-danger">*</span> <br></label>
                                     <input class="form-control" name="icon" placeholder="{{ __('services.attributes.icon') }}" value="{{$service!=null ? $service->icon : old('icon')}}" required="" type="text">
-                                    <span>You Can Get Icon Options By <a href="https://iconscout.com/unicons/free-line-icons" target="_blank">Click here</a></span>
+                                    <span>You Can Get Icon Options By <a href="https://fontawesome.com/v4/icons" target="_blank">Click here</a></span>
                                 </div>
                             </div>
 
