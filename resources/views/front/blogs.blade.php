a173446c0ca62df43bd8a23f12bb32de2845faa2
Mahmoud Maher <mahmoud_maher12345@yahoo.com>
2025-04-05 17:46:58 +0200

    update layouts

--- resources/views/front/blogs.blade.php
+++ resources/views/front/blogs.blade.php
@@ -48,12 +48,15 @@
                                         <h4><a href="{{route('blog.details', $blog->slug)}}">{{$blog->title}}</a></h4>
                                         <div class="meta-block">
                                             <ul class="inline">
-                                                <li><a href="javascript:void(0);"><i class="fa fa-calendar"
-                                                                                     aria-hidden="true"></i> {{$blog->created_at->format('d M Y')}}
-                                                    </a></li>
+                                                <li>
+                                                    <a href="javascript:void(0);">
+                                                        <i class="fa fa-calendar"
+                                                           aria-hidden="true"></i> {{$blog->created_at->format('d M Y')}}
+                                                    </a>
+                                                </li>
                                             </ul>
                                         </div>
-                                        <p>{{$blog->description}}</p>
+                                        <p>{!! \Illuminate\Support\Str::words($blog->description, 20 ) !!}</p>
                                         <a href="{{route('blog.details', $blog->slug)}}" class="read-more">read more</a>
                                     </div>
                                 </div>
